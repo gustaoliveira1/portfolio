81b3212199c4fddcb5edebc540f5e771d685d211
Gusta Oliveira <gusta01.oliveira@gmail.com>
2023-07-17 17:41:53 -0300

    feat: add wave emoji

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ export function Hero() {
   return (
     <section className="min-h-screen flex items-center">
       <div className="max-w-[1216px] w-full mx-auto p-6 flex flex-col gap-6 justify-center">
-        <h3 className="text-xl">Olá, eu sou o</h3>
+        <h3 className="text-xl">👋 Olá, eu sou o</h3>
         <h1 className="text-6xl tracking-wider font-bold">Gusta Oliveira</h1>
         <h2 className="text-3xl tracking-wide font-semibold">
           Desenvolvedor <span className="text-secondary">Fullstack</span>
