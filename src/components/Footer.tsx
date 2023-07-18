7ecbf2e2e7262ac262946d4d6ed7a109543ee9ff
Gusta Oliveira <gusta01.oliveira@gmail.com>
2023-07-18 17:19:02 -0300

    feat: create Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="w-full p-6 flex items-center justify-center bg-white">
+      <p className="text-black/70">&copy; 2023 - Todos os diretos reservados à Gusta Oliveira</p>
+    </footer>
+  )
+}
