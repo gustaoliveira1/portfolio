c46d990ec0ca883feeb0f91525eac90f57dff8f1
Gusta Oliveira <gusta01.oliveira@gmail.com>
2023-07-19 10:45:13 -0300

    feat: center text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="w-full p-6 flex items-center justify-center bg-white">
-      <p className="text-black/70">
+      <p className="text-black/70 text-center">
         &copy; 2023 - Todos os diretos reservados à Gusta Oliveira
       </p>
     </footer>
