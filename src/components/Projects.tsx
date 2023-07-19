8d7e59badf26a96dbf5e72204685e5e2cb58a3b4
Gusta Oliveira <gusta01.oliveira@gmail.com>
2023-07-19 11:23:37 -0300

    feat: add projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,8 +2,12 @@ import { ProjectPattern } from "./ProjectPattern";
 
 import gelateriaTablet from "@/assets/gelateria-talblet.png";
 import gelateriaSmartphone from "@/assets/gelateria-smartphone.png";
-import jogoDaVelhaTablet from "@/assets/jogo-da-velha-tablet.png";
-import jogoDaVelhaSmartphone from "@/assets/jogo-da-velha-smartphone.png";
+import agenciaTablet from "@/assets/agencia-tablet.png";
+import agenciaSmartphone from "@/assets/agencia-smartphone.png";
+import alphatechTablet from "@/assets/alphatech-tablet.png"
+import alphatechSmartphone from "@/assets/alphatech-smartphone.png"
+import cmlifeTablet from "@/assets/cmlife-tablet.png"
+import cmlifeSmartphone from "@/assets/cmlife-smatphone.png"
 
 export function Projects() {
   return (
@@ -24,8 +28,22 @@ export function Projects() {
             title="Jogo da velha"
             description="Um simples jogo da velha para se divertir com os amigos!"
             technologies={["React", "Next", "Typescript", "TailwindCss"]}
-            smartphoneImage={jogoDaVelhaSmartphone}
-            tabletImage={jogoDaVelhaTablet}
+            smartphoneImage={agenciaSmartphone}
+            tabletImage={agenciaTablet}
+          />
+          <ProjectPattern
+            title="Gelateria"
+            description="Landing page criada para exibir os deliciosos sorvetes artesanais da tradicional Gelateria."
+            technologies={["React", "Javascript", "CSS"]}
+            smartphoneImage={alphatechSmartphone}
+            tabletImage={alphatechTablet}
+          />
+          <ProjectPattern
+            title="Jogo da velha"
+            description="Um simples jogo da velha para se divertir com os amigos!"
+            technologies={["React", "Next", "Typescript", "TailwindCss"]}
+            smartphoneImage={cmlifeSmartphone}
+            tabletImage={cmlifeTablet}
           />
         </div>
       </div>
